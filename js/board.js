143e651b9added96d9f1c3b66f7f9c113698ec48
Nikola Nikolov <nikola_00219@abv.bg>
2014-11-18 15:03:57 +0200

    player bullets speed

--- js/board.js
+++ js/board.js
@@ -264,14 +264,14 @@ var player = {
             player.fireInterval = 3;
             bulletSound.play();
             if (player.doubleGuns === true) {
-                Game.bullets.push(new Bullet('player', 10, player.speed,
+                Game.bullets.push(new Bullet('player', 10, player.speed + 2,
                             0, player.positionX + player.width - 15, player.positionY + 10));
-                Game.bullets.push(new Bullet('player', 10, player.speed,
+                Game.bullets.push(new Bullet('player', 10, player.speed + 2,
                             0, player.positionX + player.width - 15, player.positionY + player.height - 10));
 
             }
             else {
-                Game.bullets.push(new Bullet('player', 10, player.speed,
+                Game.bullets.push(new Bullet('player', 10, player.speed + 2,
                             0, player.positionX + player.width - 15,
                             player.positionY + player.height / 2));
             }
